55f759ea4a2a4475de382964b98975b3d4f7e39a
Ranoth <account@ranoth.com>
2025-06-27 19:41:12 +0200

    fix: ci

--- src/lib/stores/gitReposStore.ts
+++ src/lib/stores/gitReposStore.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_GITHUB_API_URL, PUBLIC_GITHUB_USERNAME } from "$env/static/public";
+// import { PUBLIC_GITHUB_API_URL, PUBLIC_GITHUB_USERNAME } from "$env/static/public";
 import type { Repository } from "$lib/types/gitRepo";
 import { writable } from "svelte/store";
 
@@ -6,7 +6,7 @@ export const gitRepos = writable<Repository[]>([]);
 
 export const fetchRepos = async () => {
 	const res = await fetch(
-		`${PUBLIC_GITHUB_API_URL}/users/${PUBLIC_GITHUB_USERNAME}/repos`
+		`${process.env.PUBLIC_GITHUB_API_URL}/users/${process.env.PUBLIC_GITHUB_USERNAME}/repos`
 	);
 
 	const data = await res.json();
